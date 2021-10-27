35cbcbd3f3189eef9c89397f2a7a97bc62d6c8c1
Temurbek <86568254+timka1996@users.noreply.github.com>
2021-10-27 17:53:12 +0500

    Add files via upload

---
+++ src/Pages/Cabinet.js
@@ -0,0 +1,19 @@
+function Cabinet({props,Users}) {
+    console.log(props)
+    return(
+        <div className={'row'}>
+            <div className="col-md-4 offset-4">
+                <div className="cabinet">
+                    <div className="card">
+                        <div className="card-header bg-primary text-white">
+                            <h1>developer21</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+
+}
+export default Cabinet
